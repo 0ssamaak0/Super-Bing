e584acee3863b3995b3ed8bf068c8a61264424fc
Usama Ahmed <53372259+0ssamaak0@users.noreply.github.com>
2023-11-24 22:59:49 +0200

    updating query selectors

--- content.js
+++ content.js
@@ -1,13 +1,13 @@
 // wait 5 seconds for the page to load
 setTimeout(function () {
     // get the search box
-    var searchBox = document.querySelectorAll(".cib-serp-main")[0].shadowRoot.querySelector("#cib-action-bar-main").shadowRoot.querySelector("#searchbox");
+    
+    var searchBox = document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("#cib-action-bar-main").shadowRoot.querySelector("div > div.main-container > div > div.input-row > cib-text-input").shadowRoot.querySelector("#searchbox");
     // set the max length to 25000
     searchBox.maxLength = 25000;
 
     // get the letter counter
-    var letterCounter = document.querySelectorAll(".cib-serp-main")[0].shadowRoot.querySelector("#cib-action-bar-main").shadowRoot.querySelector(".letter-counter").childNodes[2];
-
+    var letterCounter = document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("#cib-action-bar-main").shadowRoot.querySelector("div > div.main-container > div > div.bottom-controls > div.bottom-right-controls > div.letter-counter").childNodes[3];
     // set the letter counter to 25000
     letterCounter.textContent = 25000;
 }, 5000);
